857eaf3b3821c5b5d4eb75dd80af9192c1fd2681
yogi <ychaudhuri5@gmail.com>
2023-03-18 00:26:45 -0500

    added services controller, services repository and services model

---
+++ SpyDuh-Celtics/Models/Services.cs
@@ -0,0 +1,10 @@
+﻿namespace SpyDuh_Celtics.Models
+{
+    public class Services
+    {
+        public int Id { get; set; }
+        public string Service { get; set; }
+        public string UserId { get; set; }
+
+    }
+}
---
+++ SpyDuh-Celtics/Repositories/BaseRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace SpyDuh_Celtics.Repositories
+{
+    /// <summary>
+    ///  A base class for every other Repository class to inherit from.
+    ///  This class is responsible for providing a database connection to each of the repository subclasses
+    /// </summary>
+    public class BaseRepository
+    {
+        /// <summary>
+        ///  A "connection string" is the address of the database.
+        /// </summary>
+        private string _connectionString;
+
+
+        /// <summary>
+        ///  This constructor will be invoked by subclasses.
+        ///  It will save the connection string for later use.
+        /// </summary>
+        public BaseRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("LocalConnection");
+        }
+
+
+        /// <summary>
+        ///  Represents a connection to the database.
+        ///   This is a "tunnel" to connect the application to the database.
+        ///   All communication between the application and database passes through this connection.
+        /// </summary>
+        protected SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_connectionString);
+            }
+        }
+
+    }
+}
