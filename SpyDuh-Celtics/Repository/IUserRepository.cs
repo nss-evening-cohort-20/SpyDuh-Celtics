9576d07f3e5af3a723917d19d8812c0eefa98ff7
ChaseBurnett <chaseburnett@me.com>
2023-03-20 18:30:08 -0500

    IBeanVarietyRepository

---
+++ SpyDuh-Celtics/Repository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using SpyDuh_Celtics.Models;
+
+namespace SpyDuh_Celtics.Repository
+{
+    public interface IUserRepository
+    {
+        void Add(User user);
+        void Delete(int id);
+        User Get(int id);
+        User Get(string Name);
+        List<User> GetAll();
+        void Update(User user);
+    }
+}
